2961aefbe15f65dc94a30d0498ab4760abaaf23c
pranaybyram <124740553+pranaybyram@users.noreply.github.com>
2023-02-07 16:30:41 -0600

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,10 @@
 
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int a =8;
+        int b=10;
+        int c = a*b;
+        System.out.println("The product of two numbers is "+c);
         // checkout -b UserStory03
         //hii
 
